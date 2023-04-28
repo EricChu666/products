6a961772810ba9a293d0586bafbd06df11e1db1f
Eric Chu <ericchu0826@gmail.com>
2023-04-28 15:36:33 +0800

    list details

--- purchase.py
+++ purchase.py
@@ -1,9 +1,12 @@
 products = []
 while True: 
-	name = input('Please enter the name of the product:')
-	if name == 'q':
-		break
-	price = input('Please enter the price of the product:')
-	products.append([name, price])
+    name = input('Please enter the name of the product:')
+    if name == 'q':
+        break
+    price = input('Please enter the price of the product:')
+    products.append([name, price])
 
 print(products)
+
+for p in products:
+    print('the price of', p[0], 'is:', p[1])
