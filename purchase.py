e1a5fbcfe7d63c9d8469f10cf48022eb9c4fd603
Eric Chu <ericchu0826@gmail.com>
2023-04-28 23:45:24 +0800

    add header

--- purchase.py
+++ purchase.py
@@ -10,3 +10,8 @@ print(products)
 
 for p in products:
     print('the price of', p[0], 'is:', p[1])
+
+with open('Products_list.csv', 'w', encoding = 'utf-8') as f:
+    f.write('name, price\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
